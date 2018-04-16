becab0caa7d16d9797b2fed21176c0b67ab90752
liweihan <weihanli@outlook.com>
2018-04-16 10:27:17 +0800

    add PrototypePattern/README.md and FactoryMethodPattern/README.md

---
+++ CreatePattern/PrototypePattern/Prototype.cs
@@ -0,0 +1,23 @@
+﻿namespace PrototypePattern
+{
+    internal abstract class Prototype
+    {
+        public string Id { get; }
+
+        protected Prototype(string id) => Id = id;
+
+        public abstract Prototype Clone();
+    }
+
+    internal class ConcretePrototype1 : Prototype
+    {
+        public ConcretePrototype1(string id) : base(id)
+        {
+        }
+
+        public override Prototype Clone()
+        {
+            return (Prototype)MemberwiseClone();
+        }
+    }
+}
---
+++ CreatePattern/PrototypePattern/README.md
@@ -0,0 +1,20 @@
+﻿# 原型模式 Prototype
+
+## Intro 简介
+
+> 原型模式，用原型实例指定创建对象的种类，并且通过拷贝这些原型创建新的对象
+
+原型模式其实就是从一个对象再创建另外一个可定制的对象而且不需要知道任何创建的细节。
+
+## 实现方式
+
+实现方式大致如下：
+
+``` csharp
+// 浅复制
+public object Clone() => MemberwiseClone();
+```
+
+## More
+
+更多设计模式及示例代码 [传送门](https://github.com/WeihanLi/DesignPatterns)
