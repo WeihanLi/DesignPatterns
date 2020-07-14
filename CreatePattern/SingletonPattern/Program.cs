0757de65a5e030a72afebcd5f30ba820f0ad45d8
weihanli <weihanli@outlook.com>
2020-07-15 01:22:39 +0800

    add Lazy Singleton sample

--- CreatePattern/SingletonPattern/Program.cs
+++ CreatePattern/SingletonPattern/Program.cs
@@ -19,22 +19,28 @@ namespace SingletonPattern
             // var i2 = Singleton2.GetInstance();
             // Console.WriteLine($"Singleton2 {ReferenceEquals(i2, Singleton2.GetInstance())}");
 
-
             Console.WriteLine($"Singleton");
-            Enumerable.Range(1, 10).Select(i=> Task.Run(()=>{
+            Enumerable.Range(1, 10).Select(i => Task.Run(() =>
+            {
                 Console.WriteLine($"{Singleton.GetInstance().GetHashCode()}");
             })).WhenAll().Wait();
 
-
             Console.WriteLine($"Singleton1");
-            Enumerable.Range(1, 10).Select(i=> Task.Run(()=>{
+            Enumerable.Range(1, 10).Select(i => Task.Run(() =>
+            {
                 Console.WriteLine($"{Singleton1.GetInstance().GetHashCode()}");
             })).WhenAll().Wait();
 
-
             Console.WriteLine($"Singleton2");
-            Enumerable.Range(1, 10).Select(i=> Task.Run(()=>{
+            Enumerable.Range(1, 10).Select(i => Task.Run(() =>
+            {
                 Console.WriteLine($"{Singleton2.GetInstance().GetHashCode()}");
+            })).WhenAll().Wait();
+
+            Console.WriteLine($"Singleton3");
+            Enumerable.Range(1, 10).Select(i => Task.Run(() =>
+            {
+                Console.WriteLine($"{Singleton3.GetInstance().GetHashCode()}");
             })).WhenAll().Wait();
 
             Console.ReadLine();
---
+++ CreatePattern/SingletonPattern/Singleton3.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SingletonPattern
+{
+    /// <summary>
+    /// 使用 Lazy 实现的单例方法，用到的时候再去实例化
+    /// </summary>
+    public class Singleton3
+    {
+        private static readonly Lazy<Singleton3>
+            LazyInstance = new Lazy<Singleton3>
+                (() => new Singleton3());
+
+        private Singleton3()
+        {
+        }
+
+        public static Singleton3 GetInstance() => LazyInstance.Value;
+    }
+}
