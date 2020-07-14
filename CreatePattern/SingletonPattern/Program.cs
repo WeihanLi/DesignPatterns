4adb1cbd377484404bb6e4da6ccc9b8a76d929a9
weihanli <weihanli@outlook.com>
2020-07-15 00:43:24 +0800

    add concurrentTestSample

--- CreatePattern/SingletonPattern/Program.cs
+++ CreatePattern/SingletonPattern/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using WeihanLi.Extensions;
 
 namespace SingletonPattern
 {
@@ -6,15 +9,33 @@ namespace SingletonPattern
     {
         public static void Main(string[] args)
         {
-            //0
-            var i0 = Singleton.GetInstance();
-            Console.WriteLine($"Singleton {ReferenceEquals(i0, Singleton.GetInstance())}");
-            //1
-            var i1 = Singleton1.GetInstance();
-            Console.WriteLine($"Singleton1 {ReferenceEquals(i1, Singleton1.GetInstance())}");
-            //2
-            var i2 = Singleton2.GetInstance();
-            Console.WriteLine($"Singleton2 {ReferenceEquals(i2, Singleton2.GetInstance())}");
+            // //0
+            // var i0 = Singleton.GetInstance();
+            // Console.WriteLine($"Singleton {ReferenceEquals(i0, Singleton.GetInstance())}");
+            // //1
+            // var i1 = Singleton1.GetInstance();
+            // Console.WriteLine($"Singleton1 {ReferenceEquals(i1, Singleton1.GetInstance())}");
+            // //2
+            // var i2 = Singleton2.GetInstance();
+            // Console.WriteLine($"Singleton2 {ReferenceEquals(i2, Singleton2.GetInstance())}");
+
+
+            Console.WriteLine($"Singleton");
+            Enumerable.Range(1, 10).Select(i=> Task.Run(()=>{
+                Console.WriteLine($"{Singleton.GetInstance().GetHashCode()}");
+            })).WhenAll().Wait();
+
+
+            Console.WriteLine($"Singleton1");
+            Enumerable.Range(1, 10).Select(i=> Task.Run(()=>{
+                Console.WriteLine($"{Singleton1.GetInstance().GetHashCode()}");
+            })).WhenAll().Wait();
+
+
+            Console.WriteLine($"Singleton2");
+            Enumerable.Range(1, 10).Select(i=> Task.Run(()=>{
+                Console.WriteLine($"{Singleton2.GetInstance().GetHashCode()}");
+            })).WhenAll().Wait();
 
             Console.ReadLine();
         }
